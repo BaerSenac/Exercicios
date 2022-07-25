fb8fb999637d46042c2338366d3d6905320efce4
JBAER <368271Qualidade!>
2022-07-25 18:00:37 -0300

    update estudos js

---
+++ Dia1/Variaveis_Tipos_Operadores/tipoDados.js
@@ -0,0 +1,21 @@
+//Numbers
+
+//const number = 5;
+
+//console.log(number.toString());
+//console.log(typeof number.toString())
+
+//Boolean
+
+false 
+true
+
+console.log(2 == 3)   //false pois 2 ñ é igual a 3
+
+console.log(2 == 2)  //true pois 2 é igual a 2
+
+//Null & Undefined
+
+const x = null;      //variavel vazia , que nao possui valor ou está vazia
+
+const y = undefined; //variavel consta que esta indefinida, nao tem valor
---
+++ Dia1/Variaveis_Tipos_Operadores/variaveis.js
@@ -0,0 +1,10 @@
+const firstName = 'Jonathan'
+const lastName = 'Baer'
+
+const names = 'Joao,Julia,Bruno'
+
+console.log('Meu nome é ' + firstName + ' ' + lastName)
+console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLocaleLowerCase()}`);
+
+console.log(names.split(","))
+
